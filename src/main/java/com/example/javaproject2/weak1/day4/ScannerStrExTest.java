bbb6c2fc4f7fe4d707c5db8bf0ea8f3e0102f8cd
choiseonyoung <bviibb@naver.com>
2023-04-20 09:39:03 +0900

    feat : 	문자열 두개 받아서 합치기

---
+++ src/main/java/com/example/javaproject2/weak1/day4/ScannerStrExTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.weak1.day4;
+
+public class ScannerStrExTest {
+    public static void main(String[] args) {
+        ScannerStrEx scannerStrEx = new ScannerStrEx();
+        scannerStrEx.inputStringsAndPlus();
+    }
+}
