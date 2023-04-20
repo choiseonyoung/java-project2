7868d466e84dc2098448916179c31929e5a2aa10
choiseonyoung <bviibb@naver.com>
2023-04-20 09:26:17 +0900

    feat : CodeUp 1001 ~ 1005

---
+++ src/main/java/com/example/javaproject2/weak1/day4/ScannerReview.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.weak1.day4;
+
+import java.util.Scanner;
+
+public class ScannerReview {
+    public void inputTwoNumbersAndPlus() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
+}
---
+++ src/main/java/com/example/javaproject2/weak1/day4/ScannerReviewTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.weak1.day4;
+
+public class ScannerReviewTest {
+    public static void main(String[] args) {
+        ScannerReview sr = new ScannerReview();
+        sr.inputTwoNumbersAndPlus();
+    }
+}
