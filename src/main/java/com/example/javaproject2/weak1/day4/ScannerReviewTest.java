5503d68f20493aaf1a19ed598d7c5de2ec0ab4cd
choiseonyoung <bviibb@naver.com>
2023-04-20 09:27:45 +0900

    Revert "feat : CodeUp 1001 ~ 1005"
    This reverts commit 7868d466e84dc2098448916179c31929e5a2aa10.

--- src/main/java/com/example/javaproject2/weak1/day4/ScannerReviewTest.java
+++
@@ -1,8 +0,0 @@
-package com.example.javaproject2.weak1.day4;
-
-public class ScannerReviewTest {
-    public static void main(String[] args) {
-        ScannerReview sr = new ScannerReview();
-        sr.inputTwoNumbersAndPlus();
-    }
-}
