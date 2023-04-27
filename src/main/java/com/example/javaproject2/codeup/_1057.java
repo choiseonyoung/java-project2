87cc781d07cf156a6badacce3ea47444d51881c5
choiseonyoung <bviibb@naver.com>
2023-04-27 22:12:18 +0900

    feat : 연산자(비교,삼항,논리,비트), CodeUp 1047 ~ 1064 등

---
+++ src/main/java/com/example/javaproject2/codeup/_1057.java
@@ -0,0 +1,17 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class _1057 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int in1 = sc.nextInt();
+        int in2 = sc.nextInt();
+
+        if(in1 == in2) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+    }
+}
---
+++ src/main/java/com/example/javaproject2/weak2/day4/Accumulate687.java
@@ -0,0 +1,18 @@
+package com.example.javaproject2.weak2.day4;
+
+public class Accumulate687 {
+    public static void main(String[] args) {
+        int num = 687;
+        int answer = 0;
+
+        answer = answer + num % 10;
+        System.out.printf("answer:%d\n", answer);
+        num = num / 10;
+        answer = answer + num % 10;
+        System.out.printf("answer:%d\n", answer);
+        num = num / 10;
+        answer = answer + num % 10;
+        System.out.printf("answer:%d\n", answer);
+    }
+
+}
